2e257d284de81d4de275aec6e855efa7f7ebdfb3
Sergey Nazarov <mr.mystic@inbox.ru>
2021-12-16 18:31:45 +0300

    Update StdioLogger.php
    Add timestamps and format

--- src/StdioLogger.php
+++ src/StdioLogger.php
@@ -12,7 +12,7 @@ use function WyriHaximus\PSR3\processPlaceHolders;
 
 final class StdioLogger extends AbstractLogger
 {
-    const NEW_LINE = \PHP_EOL;
+    const NEW_LINE = PHP_EOL;
 
     /**
      * @var Stdio
@@ -30,6 +30,16 @@ final class StdioLogger extends AbstractLogger
     private $newLine = false;
 
     /**
+     * @var bool
+     */
+    private $timeStamps = false;
+
+    /**
+     * @var string
+     */
+    private $timeStampFormat = DATE_RFC3339;
+
+    /**
      * @param WritableStreamInterface $stream
      *
      * @internal
@@ -44,7 +54,7 @@ final class StdioLogger extends AbstractLogger
      */
     public static function create(LoopInterface $loop): StdioLogger
     {
-        return new self(new WritableResourceStream(\STDOUT, $loop));
+        return new self(new WritableResourceStream(STDOUT, $loop));
     }
 
     public function withHideLevel(bool $hideLevel): StdioLogger
@@ -63,7 +73,23 @@ final class StdioLogger extends AbstractLogger
         return $clone;
     }
 
-    public function log($level, $message, array $context = []): void
+    public function withTimeStamps(bool $timeStamps): StdioLogger
+    {
+        $clone = clone $this;
+        $clone->timeStamps = $timeStamps;
+
+        return $clone;
+    }
+
+    public function setTimeStampFormat(string $format): StdioLogger
+    {
+        $clone = clone $this;
+        $clone->timeStampFormat = $format;
+
+        return $clone;
+    }
+
+    public function log($level, $message, array $context = [])
     {
         checkCorrectLogLevel($level);
         $message = (string)$message;
@@ -73,6 +99,9 @@ final class StdioLogger extends AbstractLogger
         }
         if ($this->newLine === true) {
             $message .= self::NEW_LINE;
+        }
+        if ($this->timeStamps === true) {
+            $message = date($this->timeStampFormat) . ' ' . $message;
         }
         $this->stdio->write($message);
     }
